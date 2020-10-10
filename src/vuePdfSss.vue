f9a148f903c558de7647c021909b27a944d09784
廖毅 <firmliao789@126.com>
2020-10-10 18:58:20 +0800

    修复vue 404问题

--- src/vuePdfSss.vue
+++ src/vuePdfSss.vue
@@ -1,23 +1,26 @@
 <style src="./annotationLayer.css"></style>
 <script>
 
-	import componentFactory from './componentFactory'
-	import pdfjsWrapper from './pdfjsWrapper'
+    import componentFactory from './componentFactory.js'
+    // 修改部分：引入pdfjsworker
+    import PdfjsWorker from 'pdfjs-dist/build/pdf.worker.js'
 
-	var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
+    if ( process.env.VUE_ENV !== 'server' ) {
 
-	if ( process.env.VUE_ENV !== 'server' ) {
+        var pdfjsWrapper = require('./pdfjsWrapper.js').default;
+        var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
 
-		if ( typeof window !== 'undefined' && 'Worker' in window ) {
+        if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
+            // 修改部分：注释原本的引入方法
+            // var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
+            PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+        }
 
-			var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
-			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
-		}
-	}
+        var component = componentFactory(pdfjsWrapper(PDFJS));
+    } else {
 
-	var component = componentFactory(pdfjsWrapper(PDFJS));
-	component.PDFJS = PDFJS;
+        var component = componentFactory({});
+    }
 
-	export default component;
-
+    export default component;
 </script>
